d8eb31170f1b84fe3a12c4b52152232d597e0a61
Anthony <amcsweeney@logitech.com>
2025-12-12 13:11:08 +0000

    Initial commit

---
+++ Data/SQL Query Folder/SQL_Queries - Claim.sql
@@ -0,0 +1,2 @@
+select * from apps.LMTC_CLAIM_OFFER_DETAILS_BOT_V
+WHERE claim_number IN (@CLAIMNUM)
---
+++ Data/SQL Query Folder/SQL_Queries - Offer.sql
@@ -0,0 +1,2 @@
+select * from apps.LMTC_CLAIM_OFFER_DETAILS_BOT_V 
+where OFFER_CODE IN (@OfferCode)
