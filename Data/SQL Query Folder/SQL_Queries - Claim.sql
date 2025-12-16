145ffb3b6980867790274c14cbe6425022aa1024
Anthony <amcsweeney@logitech.com>
2025-12-16 09:30:46 +0000

    Portal Check Added and Asana Updates

--- Data/SQL Query Folder/SQL_Queries - Claim.sql
+++
@@ -1,2 +0,0 @@
-select * from apps.LMTC_CLAIM_OFFER_DETAILS_BOT_V
-WHERE claim_number IN (@CLAIMNUM)
--- Data/SQL Query Folder/SQL_Queries - Offer.sql
+++
@@ -1,2 +0,0 @@
-select * from apps.LMTC_CLAIM_OFFER_DETAILS_BOT_V 
-where OFFER_CODE IN (@OfferCode)
